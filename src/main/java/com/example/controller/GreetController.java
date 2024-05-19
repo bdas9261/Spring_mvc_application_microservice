36a92a26fde4ced1d5dbea41ad5a937ffa57c095
biswanath <bdas9261@gmail.com>
2024-05-20 00:48:08 +0530

    added spring mvc microcevice

---
+++ src/main/java/com/example/controller/GreetController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetController {
+	@RequestMapping("/sayHello")
+   public String hellouser(Model model	) {
+		model.addAttribute("message","have a good day");
+	return "Sucess";
+	   
+   }
+}
